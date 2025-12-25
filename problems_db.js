166a0acb76ec114cd816a64129584ec657adce5a
greantean999 <occhiali.notte3574@gmail.com>
2025-12-25 18:12:46 +0900

    Update problems_db.js

--- problems_db.js
+++ problems_db.js
@@ -1,8 +1,5 @@
-// MasterDatabase の初期化
-const MasterDatabase = {};
-Object.assign(MasterDatabase, {
-    // 大問1(1) 正負の数・文字式 【20題】
+const MasterDatabase = {
     d1_q1: [
-        { q: String.raw`$1 - 6 \times (-2)$`, a: String.raw`$13$`, e: String.raw`$1 - (-12) = 13$` },
-       
-});
+        { q: "1 + 1 は？", a: "2", e: "計算の基本です" }
+    ]
+};
